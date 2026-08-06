470b3824dabf762345d3fe9cd5cb36a27b231cfa
Jake Zahm <jakezahm3@gmail.com>
2026-08-06 06:05:27 -0400

    3e005bad-8b4d-43a3-b956-c025776c5626

--- nvim/extraConfig.lua
+++ nvim/extraConfig.lua
@@ -11,6 +11,76 @@ vim.opt.expandtab = true
 vim.api.nvim_set_hl(0, "BlinkCmpKindSupermaven", { fg = "#7aa2f7", bold = true })
 vim.api.nvim_set_hl(0, "BlinkCmpLabelSupermaven", { fg = "#bb9af7", italic = true })
 
+local dropbar = require("dropbar")
+local sources = require("dropbar.sources")
+local utils = require("dropbar.utils")
+
+vim.api.nvim_set_hl(0, "DropBarFileName", { fg = "#FFFFFF", italic = true })
+
+local custom_path = {
+	get_symbols = function(buff, win, cursor)
+		local symbols = sources.path.get_symbols(buff, win, cursor)
+		symbols[#symbols].name_hl = "DropBarFileName"
+		if vim.bo[buff].modified then
+			symbols[#symbols].name = symbols[#symbols].name .. " [+]"
+			symbols[#symbols].name_hl = "DiffAdded"
+		end
+		return symbols
+	end,
+}
+
+require("dropbar").setup({
+	bar = {
+		enable = function(buf, win, _)
+			buf = vim._resolve_bufnr(buf)
+			if not vim.api.nvim_buf_is_valid(buf) or not vim.api.nvim_win_is_valid(win) then
+				return false
+			end
+
+			if
+				not vim.api.nvim_buf_is_valid(buf)
+				or not vim.api.nvim_win_is_valid(win)
+				or vim.fn.win_gettype(win) ~= ""
+				or vim.wo[win].winbar ~= ""
+				or vim.bo[buf].ft == "help"
+			then
+				return false
+			end
+
+			local stat = vim.uv.fs_stat(vim.api.nvim_buf_get_name(buf))
+			if stat and stat.size > 1024 * 1024 then
+				return false
+			end
+
+			return vim.bo[buf].bt == "terminal"
+				or vim.bo[buf].ft == "markdown"
+				or pcall(vim.treesitter.get_parser, buf)
+				or not vim.tbl_isempty(vim.lsp.get_clients({
+					bufnr = buf,
+					method = "textDocument/documentSymbol",
+				}))
+		end,
+	},
+	sources = {
+		path = {
+			relative_to = function(buf, win)
+				-- Show full path in oil or fugitive buffers
+				local bufname = vim.api.nvim_buf_get_name(buf)
+				if vim.startswith(bufname, "oil://") or vim.startswith(bufname, "fugitive://") then
+					local root = bufname:gsub("^%S+://", "", 1)
+					while root and root ~= vim.fs.dirname(root) do
+						root = vim.fs.dirname(root)
+					end
+					return root
+				end
+
+				local ok, cwd = pcall(vim.fn.getcwd, win)
+				return ok and cwd or vim.fn.getcwd()
+			end,
+		},
+	},
+})
+
 vim.lsp.config("nixd", {})
 vim.lsp.config("pyright", {})
 vim.lsp.config("rust_analyzer", {})
--- nvim/plugins.lua
+++ nvim/plugins.lua
@@ -14,6 +14,17 @@ require("base46").load_all_highlights()
 
 return {
 	{
+		"stevearc/oil.nvim",
+		---@module 'oil'
+		---@type oil.SetupOpts
+		opts = {},
+		-- Optional dependencies
+		dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+		-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+		-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+		lazy = false,
+	},
+	{
 		{
 			"rachartier/tiny-inline-diagnostic.nvim",
 			event = "VeryLazy",
