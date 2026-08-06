02cc50d8f5c28a34b1b41f52f027977b71f17a62
Jake Zahm <jakezahm3@gmail.com>
2026-08-06 13:45:13 -0400

    3d4e5f6a-f5b1-4c3c-b898-c4f51964c993

--- nvim/plugins.lua
+++ nvim/plugins.lua
@@ -56,9 +56,6 @@ return {
 			local dropbar_api = require("dropbar.api")
 			vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
 			vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
-			vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
-			vim.keymap.set("n", "[]", dropbar_api.select_prev_context, { desc = "Select previous context" })
-			vim.keymap.set("n", "]]", dropbar_api.select_next_context_end, { desc = "Select next context (til end)" })
 		end,
 	},
 	{
