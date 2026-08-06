bb3d0b5133ead0622dde8007daa2dc37574ccecf
Jake Zahm <jakezahm3@gmail.com>
2026-08-05 20:15:06 -0400

    d780ee49-4ef5-4eb9-b71c-0bef0c95f287

--- home.nix
+++ home.nix
@@ -58,10 +58,11 @@
   programs.nvchad = {
     backup = false;
     enable = true;
-    neovim = pkgs.neovim.override {
+    neovim = pkgs.wrapNeovim pkgs.neovim-unwrapped {
       withNodeJs = true;
       withPython3 = true;
     };
+
     extraPackages = with pkgs; [
       ripgrep
       lua-language-server
@@ -82,7 +83,6 @@
       git
       curl
       black
-      hadolint
       shfmt
       rustfmt
       jq
@@ -90,88 +90,70 @@
       uv
       google-java-format
       python3
+      python3Packages.pynvim
+      neovim-node-client
     ];
     extraConfig = ''
-      vim.g.rainbow_delimiters = {
-      	strategy = {
-      		[""] = "rainbow-delimiters.strategy.global",
-      		vim = "rainbow-delimiters.strategy.local",
-      	},
-      	query = {
-      		[""] = "rainbow-delimiters",
-      		lua = "rainbow-blocks",
-      	},
-      	priority = {
-      		[""] = 110,
-      		lua = 210,
-      	},
-      	highlight = {
-      		"RainbowDelimiterRed",
-      		"RainbowDelimiterYellow",
-      		"RainbowDelimiterBlue",
-      		"RainbowDelimiterOrange",
-      		"RainbowDelimiterGreen",
-      		"RainbowDelimiterViolet",
-      		"RainbowDelimiterCyan",
-      	},
-      }
-            -- Custom vim options
-             vim.opt.shiftwidth = 2
-             vim.opt.tabstop = 2
-             vim.opt.expandtab = true
+      -- Provider configuration
+      vim.g.python3_host_prog = "${pkgs.python3.withPackages (p: [p.pynvim])}/bin/python3"
+      vim.g.node_host_prog = vim.fn.exepath('neovim-node-host')
+      vim.g.loaded_python3_provider = nil;
+      vim.g.loaded_node_provider = nil;
+      -- Custom vim options
+       vim.opt.shiftwidth = 2
+       vim.opt.tabstop = 2
+       vim.opt.expandtab = true
+       -- Define custom highlight groups for supermaven
+       vim.api.nvim_set_hl(0, "BlinkCmpKindSupermaven", { fg = "#7aa2f7", bold = true })
+       vim.api.nvim_set_hl(0, "BlinkCmpLabelSupermaven", { fg = "#bb9af7", italic = true })
 
-             -- Define custom highlight groups for supermaven
-             vim.api.nvim_set_hl(0, "BlinkCmpKindSupermaven", { fg = "#7aa2f7", bold = true })
-             vim.api.nvim_set_hl(0, "BlinkCmpLabelSupermaven", { fg = "#bb9af7", italic = true })
+       -- LSP configuration using new vim.lsp.config API
+       vim.lsp.config("nixd", {})
+       vim.lsp.config("pyright", {})
+       vim.lsp.config("rust_analyzer", {})
+       vim.lsp.config("ts_ls", {})
+       vim.lsp.config("lua_ls", {})
+       vim.lsp.config("html", {})
+       vim.lsp.config("cssls", {})
+       vim.lsp.config("bashls", {})
+       vim.lsp.config("yamlls", {})
+       vim.lsp.config("jsonls", {})
 
-             -- LSP configuration using new vim.lsp.config API
-             vim.lsp.config("nixd", {})
-             vim.lsp.config("pyright", {})
-             vim.lsp.config("rust_analyzer", {})
-             vim.lsp.config("ts_ls", {})
-             vim.lsp.config("lua_ls", {})
-             vim.lsp.config("html", {})
-             vim.lsp.config("cssls", {})
-             vim.lsp.config("bashls", {})
-             vim.lsp.config("yamlls", {})
-             vim.lsp.config("jsonls", {})
+       vim.lsp.enable("nixd", "pyright", "rust_analyzer", "ts_ls", "lua_ls", "html", "cssls", "bashls", "yamlls", "jsonls")
 
-             vim.lsp.enable("nixd", "pyright", "rust_analyzer", "ts_ls", "lua_ls", "html", "cssls", "bashls", "yamlls", "jsonls")
+       -- Custom keymaps
+       vim.keymap.set("n", "<leader>w", "<cmd>w<CR>", { desc = "Save file" })
 
-             -- Custom keymaps
-             vim.keymap.set("n", "<leader>w", "<cmd>w<CR>", { desc = "Save file" })
+       vim.keymap.set({ "n", "v" }, "<leader>F", function()
+         require("conform").format({
+           lsp_fallback = true,
+           async = false,
+           timeout_ms = 500,
+         })
+       end, { desc = "Format file or range (in visual mode)" })
 
-             vim.keymap.set({ "n", "v" }, "<leader>F", function()
-               require("conform").format({
-                 lsp_fallback = true,
-                 async = false,
-                 timeout_ms = 500,
-               })
-             end, { desc = "Format file or range (in visual mode)" })
-
-             -- Conform formatter configuration
-             require("conform").setup({
-               formatters_by_ft = {
-                 lua = { "stylua" },
-                 css = { "prettierd" },
-                 html = { "prettierd" },
-                 nix = { "alejandra" },
-                 json = { "jq" },
-                 yaml = { "prettierd" },
-                 javascript = { "prettierd" },
-                 rust = { "rustfmt" },
-                 python = { "black" },
-                 typescript = { "prettierd" },
-                 sh = { "shfmt" },
-                 java = { "google-java-format" },
-                 dockerfile = { "hadolint" },
-                 markdown = { "prettierd" },
-               },
-               format_on_save = {
-                 timeout_ms = 500,
-                 lsp_fallback = true,
-               },
-             })
+       -- Conform formatter configuration
+       require("conform").setup({
+         formatters_by_ft = {
+           lua = { "stylua" },
+           css = { "prettierd" },
+           html = { "prettierd" },
+           nix = { "alejandra" },
+           json = { "jq" },
+           yaml = { "prettierd" },
+           javascript = { "prettierd" },
+           rust = { "rustfmt" },
+           python = { "black" },
+           typescript = { "prettierd" },
+           sh = { "shfmt" },
+           java = { "google-java-format" },
+           markdown = { "prettierd" },
+         },
+         format_on_save = {
+           timeout_ms = 500,
+           lsp_fallback = true,
+         },
+       })
 
     '';
     chadrcConfig = ''
@@ -181,7 +163,7 @@
       M.base46 = {
       	theme = "eldritch",
 
-      	 hl_override = {
+      	 hl_ooverride = {
       	 	Comment = { italic = true },
       	 	["@comment"] = { italic = true },
       	 },
@@ -197,132 +179,181 @@
       return M
     '';
     extraPlugins = ''
-      return {
-      	{ "mason-org/mason.nvim", enabled = false },
-      	-- Disable default nvim-cmp
-      	{ "hrsh7th/nvim-cmp", enabled = false },
-      	-- Also disable cmp dependencies that might conflict
-      	{ "L3MON4D3/LuaSnip", enabled = false },
-      	{ "saadparwaiz1/cmp_luasnip", enabled = false },
-      	{ "hrsh7th/cmp-nvim-lua", enabled = false },
-      	{ "hrsh7th/cmp-nvim-lsp", enabled = false },
-      	{ "hrsh7th/cmp-buffer", enabled = false },
-      	{ "hrsh7th/cmp-path", enabled = false },
-      	{ "windwp/nvim-autopairs", enabled = true },
-      	{
-      		"onsails/lspkind.nvim",
-      		enabled = true,
-      		config = function()
-      			require("lspkind").init()
-      		end,
-      	},
-
-      	{ "nvim-lua/plenary.nvim" },
-      	{
-      		"saghen/blink.cmp",
-      		lazy = false,
-      		dependencies = {
-      			"saghen/blink.lib",
-      			-- optional: provides snippets for the snippet source
-      			"rafamadriz/friendly-snippets",
-      			{
-      				"supermaven-inc/supermaven-nvim",
-      				opts = {
-      					disable_inline_completion = true, -- disable inline completion to use blink.cmp instead
-      					disable_keymaps = false, -- keeps built in keymaps for manual control
-      					log_level = "off",
-      				},
-      			},
-      			{
-      				"huijiro/blink-cmp-supermaven",
-      			},
-      		},
-      		build = function()
-      			require("blink.cmp").build():pwait(10000)
-      		end,
-
-      		---@module 'blink.cmp'
-      		---@type blink.cmp.Config
-      		opts = {
-      			keymap = { preset = "default" },
-      			completion = {
-      				documentation = { auto_show = false },
-      				menu = {
-      					draw = {
-                  padding =  1,
-      						components = {
-      							kind_icon = {
-      								text = function(ctx)
-      									local icon = ctx.kind_icon
-      									if vim.tbl_contains({ "Path" }, ctx.source_name) then
-      										local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
-      										if dev_icon then
-      											icon = dev_icon
-      										end
-      									elseif ctx.source_name == "supermaven" then
-      										icon = "🤖"
-      									else
-      										icon = require("lspkind").symbol_map[ctx.kind] or ""
-      									end
-
-      									return icon .. ctx.icon_gap
-      								end,
-
-      								-- Optionally, use the highlight groups from nvim-web-devicons
-      								-- You can also add the same function for `kind.highlight` if you want to
-      								-- keep the highlight groups in sync with the icons.
-      								highlight = function(ctx)
-      									local hl = ctx.kind_hl
-      									if vim.tbl_contains({ "Path" }, ctx.source_name) then
-      										local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
-      										if dev_icon then
-      											hl = dev_hl
-      										end
-      									elseif ctx.source_name == "supermaven" then
-      										hl = "BlinkCmpKindSupermaven"
-      									end
-      									return hl
-      								end,
-      							},
-      							label = {
-      								text = function(ctx)
-      									local label = ctx.label
-      									if ctx.source_name == "supermaven" then
-      										return "AI: " .. label
-      									end
-      									return label
-      								end,
-      								highlight = function(ctx)
-      									if ctx.source_name == "supermaven" then
-      										return "BlinkCmpLabelSupermaven"
-      									end
-      									return ctx.label_hl
-      								end,
-      							},
-      						},
-      					},
-      				},
-      			},
-      			fuzzy = { implementation = "rust" },
-      			sources = {
-      				default = { "lsp", "path", "snippets", "buffer", "supermaven" },
-      				providers = {
-      					supermaven = {
-      						name = "supermaven",
-      						module = "blink-cmp-supermaven",
-      						async = true,
-      						transform_items = function(ctx, items)
-      							for _, item in ipairs(items) do
-      								item.source_name = "supermaven"
-      							end
-      							return items
-      						end,
-      					},
-      				},
-      			},
-      		},
-      	},
+        local enable_providers = {
+        "python3_provider",
+        "node_provider",
+        -- and so on
       }
+
+      for _, plugin in pairs(enable_providers) do
+        vim.g["loaded_" .. plugin] = nil
+        vim.cmd("runtime " .. plugin)
+      end
+        return {
+        	{ "mason-org/mason.nvim", enabled = false },
+        	-- Disable default nvim-cmp
+        	{ "hrsh7th/nvim-cmp", enabled = false },
+        	-- Also disable cmp dependencies that might conflict
+        	{ "L3MON4D3/LuaSnip", enabled = false },
+        	{ "saadparwaiz1/cmp_luasnip", enabled = false },
+        	{ "hrsh7th/cmp-nvim-lua", enabled = false },
+        	{ "hrsh7th/cmp-nvim-lsp", enabled = false },
+        	{ "hrsh7th/cmp-buffer", enabled = false },
+        	{ "hrsh7th/cmp-path", enabled = false },
+        	{ "windwp/nvim-autopairs", enabled = true },
+        	{ "nvim-mini/mini.nvim", version = false, enabled = true },
+        	{
+        		"onsails/lspkind.nvim",
+        		enabled = true,
+        		config = function()
+        			require("lspkind").init()
+        		end,
+        	},
+          {
+            "folke/noice.nvim",
+            event = "VeryLazy",
+            opts = {
+              -- add any options here
+            },
+            dependencies = {
+              "MunifTanjim/nui.nvim",
+              "rcarriga/nvim-notify",
+              },
+          },
+        	{
+        		"HiPhish/rainbow-delimiters.nvim",
+        		dependencies = "nvim-treesitter/nvim-treesitter",
+        		config = function()
+        			-- See configuration section below
+        			local rainbow_delimiters = require("rainbow-delimiters")
+
+        			vim.g.rainbow_delimiters = {
+        				strategy = {
+        					[""] = rainbow_delimiters.strategy["global"],
+        					vim = rainbow_delimiters.strategy["local"],
+        				},
+        				query = {
+        					[""] = "rainbow-delimiters",
+        					lua = "rainbow-blocks",
+        				},
+        				highlight = {
+        					"RainbowDelimiterRed",
+        					"RainbowDelimiterYellow",
+        					"RainbowDelimiterBlue",
+        					"RainbowDelimiterOrange",
+        					"RainbowDelimiterGreen",
+        					"RainbowDelimiterViolet",
+        					"RainbowDelimiterCyan",
+        				},
+        			}
+        		end,
+        	},
+        	{ "nvim-lua/plenary.nvim" },
+        	{
+        		"saghen/blink.cmp",
+        		lazy = false,
+        		dependencies = {
+        			"saghen/blink.lib",
+        			-- optional: provides snippets for the snippet source
+        			"rafamadriz/friendly-snippets",
+        			{
+        				"supermaven-inc/supermaven-nvim",
+        				opts = {
+        					disable_inline_completion = true, -- disable inline completion to use blink.cmp instead
+        					disable_keymaps = false, -- keeps built in keymaps for manual control
+        					log_level = "off",
+        				},
+        			},
+        			{
+        				"huijiro/blink-cmp-supermaven",
+        			},
+        		},
+        		build = function()
+        			require("blink.cmp").build():pwait(10000)
+        		end,
+
+        		---@module 'blink.cmp'
+        		---@type blink.cmp.Config
+        		opts = {
+        			keymap = { preset = "default" },
+        			completion = {
+        				documentation = { auto_show = false },
+        				menu = {
+        					draw = {
+        						padding = 1,
+        						components = {
+        							kind_icon = {
+        								text = function(ctx)
+        									local icon = ctx.kind_icon
+        									if vim.tbl_contains({ "Path" }, ctx.source_name) then
+        										local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
+        										if dev_icon then
+        											icon = dev_icon
+        										end
+        									elseif ctx.source_name == "supermaven" then
+        										icon = "🤖"
+        									else
+        										icon = require("lspkind").symbol_map[ctx.kind] or ""
+        									end
+
+        									return icon .. ctx.icon_gap
+        								end,
+
+        								-- Optionally, use the highlight groups from nvim-web-devicons
+        								-- You can also add the same function for `kind.highlight` if you want to
+        								-- keep the highlight groups in sync with the icons.
+        								highlight = function(ctx)
+        									local hl = ctx.kind_hl
+        									if vim.tbl_contains({ "Path" }, ctx.source_name) then
+        										local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
+        										if dev_icon then
+        											hl = dev_hl
+        										end
+        									elseif ctx.source_name == "supermaven" then
+        										hl = "BlinkCmpKindSupermaven"
+        									end
+        									return hl
+        								end,
+        							},
+        							label = {
+        								text = function(ctx)
+        									local label = ctx.label
+        									if ctx.source_name == "supermaven" then
+        										return "AI: " .. label
+        									end
+        									return label
+        								end,
+        								highlight = function(ctx)
+        									if ctx.source_name == "supermaven" then
+        										return "BlinkCmpLabelSupermaven"
+        									end
+        									return ctx.label_hl
+        								end,
+        							},
+        						},
+        					},
+        				},
+        			},
+        			fuzzy = { implementation = "rust" },
+        			sources = {
+        				default = { "lsp", "path", "snippets", "buffer", "supermaven" },
+        				providers = {
+        					supermaven = {
+        						name = "supermaven",
+        						module = "blink-cmp-supermaven",
+        						async = true,
+        						transform_items = function(ctx, items)
+        							for _, item in ipairs(items) do
+        								item.source_name = "supermaven"
+        							end
+        							return items
+        						end,
+        					},
+        				},
+        			},
+        		},
+        	},
+        }
     '';
   };
 
