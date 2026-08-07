447cd2c6295e0bfa14cd9af1f214071f3b4c69b7
Jake Zahm <jakezahm3@gmail.com>
2026-08-06 21:22:34 -0400

    2e66c767-c4cd-47a4-a195-1a67514c9a12

--- home.nix
+++ home.nix
@@ -37,6 +37,7 @@
     pkgs.python314Packages.pip
     pkgs.python314Packages.fastmcp
     pkgs.rustup
+    pkgs.npx
     inputs.claude-desktop.packages.${pkgs.system}.claude-desktop-with-fhs
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
