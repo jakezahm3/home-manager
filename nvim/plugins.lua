b3fe8c747081d3836fd02032b14ebf74b0cba20a
Jacob Zahm <jacobzahm@icloud.com>
2026-09-04 01:10:50 -0400

    9e6a2624-fa9d-49f2-8804-27653b14ec0f

--- nvim/plugins.lua
+++ nvim/plugins.lua
@@ -13,6 +13,34 @@ require("base46").load_all_highlights()
 -- onfiguration using new vim.lsp.config API
 return {
 	{
+		"neovim/nvim-lspconfig", -- REQUIRED: for native Neovim LSP integration
+		lazy = false, -- REQUIRED: tell lazy.nvim to start this plugin at startup
+		dependencies = {
+			-- main one
+			{ "ms-jpq/coq_nvim", branch = "coq" },
+
+			-- 9000+ Snippets
+			{ "ms-jpq/coq.artifacts", branch = "artifacts" },
+
+			-- lua & third party sources -- See https://github.com/ms-jpq/coq.thirdparty
+			-- Need to **configure separately**
+			{ "ms-jpq/coq.thirdparty", branch = "3p" },
+			-- - shell repl
+			-- - nvim lua api
+			-- - scientific calculator
+			-- - comment banner
+			-- - etc
+		},
+		init = function()
+			vim.g.coq_settings = {
+				-- Your COQ settings here
+			}
+		end,
+		config = function()
+			-- Your LSP settings here
+		end,
+	},
+	{
 		"stevearc/oil.nvim",
 		---@module 'oil'
 		---@type oil.SetupOpts
@@ -194,6 +222,13 @@ return {
 			"MunifTanjim/nui.nvim",
 			"rcarriga/nvim-notify",
 		},
+	},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+		-- use opts = {} for passing setup options
+		-- this is equivalent to setup({}) function
 	},
 	{
 		"nvim-treesitter/nvim-treesitter",
